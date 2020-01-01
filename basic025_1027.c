661d5769d0940e00dbc7cfb7a1df58860db5ce89
congcoding <46776761+congcoding@users.noreply.github.com>
2020-01-01 21:43:53 +0900

    Update basic025_1027.c

--- basic025_1027.c
+++ basic025_1027.c
@@ -2,7 +2,7 @@
 
 int main() {
   int y, m, d;
-  scanf("%4d.%2m.%2d", &y, &m, &d);
-  printf("%2d-%2m-%4y", d, m, y);
+  scanf("%4d.%2d.%2d", &y, &m, &d);
+  printf("%02d-%02d-%4d", d, m, y);
   return 0;
 }

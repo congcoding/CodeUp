c6fc83d78720a15468c6f3afee587a40a56ca535
congcoding <congcoding@gmail.com>
2020-01-31 00:08:33 +0900

    basic 068

---
+++ basic068_1070.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int month;
+	scanf("%d", &month);
+
+	switch (month) {
+		case 12 : 
+		case 1 : 
+		case 2: printf("winter"); break;
+		case 3 : 
+		case 4 : 
+		case 5: printf("spring"); break;
+		case 6 : 
+		case 7 : 
+		case 8: printf("summer"); break;
+		default : printf("fall");
+	}
+
+	return 0;
+}

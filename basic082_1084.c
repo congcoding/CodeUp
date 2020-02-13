3ef1dc88a97726a38b02d5efb317978586ba2bfb
congcoding <congcoding@gmail.com>
2020-02-14 08:36:10 +0900

    basic 082

---
+++ basic082_1084.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int r, g, b;
+	scanf("%d %d %d", &r, &g, &b);
+
+	int count = 0;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < g; j++) {
+			for (int k = 0; k < b; k++) {
+				printf("%d %d %d\n", i, j, k);
+				count++;
+			}
+		}
+	}
+
+	printf("%d", count);
+
+	return 0;
+}

0d9f5f575eb5c12073289ee8b79b77dfe05c4f0f
congcoding <congcoding@gmail.com>
2020-02-22 23:58:39 +0900

    basic 090

---
+++ basic090_1092.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int gcd(int x, int y) {
+	if (y == 0) return x;
+	return (y, x%y);
+}
+
+int main() {
+	int a, b, c, result;
+	scanf("%d %d %d", &a, &b, &c);
+
+	result = gcd(a, b);
+	result = gcd(result, c);
+	if (result == 0)
+		printf("%d", result);
+	else
+		printf("%d", (a * b * c) / result);
+
+	return 0;
+}

e2fcdbef705614bd1c5389ae06aa9f811bf549a7
congcoding <congcoding@gmail.com>
2020-05-15 10:15:17 +0900

    basic 094

---
+++ basic094_1096.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int n, x, y;
+	int a[20][20] = {};
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &x, &y);
+		a[x][y] = 1;
+	}
+	for (int i = 1; i <= 19; i++) {
+		for (int j = 1; j <= 19; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
